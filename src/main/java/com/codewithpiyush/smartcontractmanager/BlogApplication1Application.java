7614346b9e8e42945ddd66385a60270f371b5812
PIYUSH BHIWAPURKAR <35625860+PiyushBhiwapurkar@users.noreply.github.com>
2022-04-13 11:21:03 +0530

    First Commit.

---
+++ src/main/java/com/codewithpiyush/smartcontractmanager/BlogApplication1Application.java
@@ -0,0 +1,13 @@
+package com.codewithpiyush.smartcontractmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/codewithpiyush/smartcontractmanager/BlogApplication1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithpiyush.smartcontractmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplication1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
